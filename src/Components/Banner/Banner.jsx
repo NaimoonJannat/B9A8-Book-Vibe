b2f3e671a59d37fa6ef212ef103e83fcd4153bce
NaimoonJannat <naimoonjannat@gmail.com>
2024-03-26 22:32:49 +0600

    MAde responsive

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 const Banner = () => {
     return (
-        <div className="hero lg:my-10 lg:py-20 max-h-screen bg-[#1313130D] rounded-3xl">
-            <div className="hero-content lg:w-4/5 mx-auto gap-20 flex-col lg:flex-row-reverse">
+        <div className="hero my-6 lg:my-10 lg:py-20 bg-[#1313130D] rounded-3xl">
+            <div className="hero-content w-full lg:w-4/5 mx-auto gap-20 flex-col lg:flex-row-reverse">
                 <img
                     src="https://i.ibb.co/gZgHXxj/twilight.jpg"
-                    className="max-w-sm rounded-lg shadow-3xl"
+                    className="lg:max-w-sm rounded-lg shadow-3xl"
                 />
                 <div>
-                    <h1 className="text-6xl font-semibold">
+                    <h1 className="text-4xl lg:text-6xl font-semibold">
                         Books to freshen up your bookshelf
                     </h1>
 
