04d89a4f808392e7c41846873888b871f67aa7d8
NaimoonJannat <naimoonjannat@gmail.com>
2024-03-26 23:43:44 +0600

    Cards responsive done

--- src/Components/Book/Book.jsx
+++ src/Components/Book/Book.jsx
@@ -3,22 +3,20 @@ const Book = ({book}) => {
     const {bookName, author, image, category, rating, tags} = book;
     return (
         <div className="card border-[#2828281A] border-2 rounded-3xl bg-base-100 shadow-xl">
-          <figure className="lg:px-6 lg:pt-6">
-            <div className="bg-[#F3F3F3] w-full rounded-3xl flex justify-center items-center text-center py-6">
-            <img src={image} className="lg:h-60" />
+          <figure className="lg:px-6 p-6 lg:pt-6">
+            <div className="bg-[#F3F3F3] w-full rounded-3xl flex justify-center items-center text-center p-8 lg:p-4">
+            <img src={image} className="h-48 lg:h-60" />
             </div>
             
           </figure>
           <div className="card-body text-left">
-          <div className="flex flex-row gap-2">
-        {/* Map over tags and render each as a button */}
+          <div className="flex flex-wrap flex-row lg:gap-2">
+        
         {tags.map((tag, index) => (
           <button
             key={index}
-            className="bg-[#23BE0A0D] btn text-[#23BE0A] p-3 font-semibold rounded-3xl text-base"
-          >
-            {tag}
-          </button>
+            className="bg-[#23BE0A0D] btn text-[#23BE0A] lg:p-3 font-semibold rounded-3xl text-xs"
+          >{tag}</button>
         ))}
       </div>
             <h2 className="text-2xl font-bold">{bookName}</h2>
