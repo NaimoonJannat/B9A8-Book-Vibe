78be552b2c0758d7c5318d0a18fff14b145621f4
NaimoonJannat <naimoonjannat@gmail.com>
2024-03-26 23:34:44 +0600

    cards section done

--- src/Components/Books/Books.jsx
+++ src/Components/Books/Books.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+import Book from "../Book/Book";
 
 const Books = () => {
+    const [books, setBooks] = useState([]);
+    useEffect( () =>{
+        fetch('bookData.json')
+        .then(res => res.json())
+        .then(data => setBooks(data));
+    } ,[])
     return (
-        <div>
+        <div className="text-center lg:my-10">
+            <h2 className="text-4xl font-bold">Books: {books.length}</h2>
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+                {
+                    books.map(book => <Book key="book.id" book={book}></Book>)
+                }
+
+            </div>
             
         </div>
     );
