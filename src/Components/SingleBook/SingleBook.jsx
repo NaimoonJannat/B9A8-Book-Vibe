8da7334387b2566cf560b22aca8aec8f05c55e48
NaimoonJannat <naimoonjannat@gmail.com>
2024-03-27 12:49:30 +0600

    Reading cards responsive

--- src/Components/SingleBook/SingleBook.jsx
+++ src/Components/SingleBook/SingleBook.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const SingleBook = ({book}) => {
     const {image, id, bookName, author, tags, category,rating,totalPages,yearOfPublishing,publisher} = book;
     return (
-        <div className="card border-[#2828281A] flex flex-row border-2 rounded-3xl bg-base-100 shadow-xl">
+        <div className="card border-[#2828281A] flex flex-col lg:flex-row border-2 rounded-3xl bg-base-100 shadow-xl">
           <figure className="lg:px-6 p-6 lg:pt-6">
             <div className="bg-[#F3F3F3] w-full rounded-3xl flex justify-center items-center text-center p-8 lg:p-4">
             <img src={image} className="h-52 lg:h-60" />
@@ -15,13 +15,13 @@ const SingleBook = ({book}) => {
           <div className="card-body text-left">
             <h2 className="text-2xl font-bold">{bookName}</h2>
             <p className="text-[#131313CC] text-base">By: {author}</p>
-            <div className="flex justify-between">
+            <div className="flex justify-between flex-col lg:flex-row">
             <div className="flex flex-wrap flex-row lg:gap-2">
         
         {tags.map((tag, index) => (
           <button
             key={index}
-            className="bg-[#23BE0A0D] text-[#23BE0A] font-semibold rounded-3xl text-xs"
+            className="bg-[#23BE0A0D] p-1 text-[#23BE0A] font-semibold rounded-3xl text-xs"
           >#{tag}</button>
         ))}
       </div>
@@ -30,7 +30,7 @@ const SingleBook = ({book}) => {
                     <p>Year of Publishing: {yearOfPublishing}</p>
                 </div>
             </div>
-            <div className="flex justify-between">
+            <div className="flex justify-between flex-col lg:flex-row">
                 <div className="flex  gap-2 justify-center items-center">
                     <IoPeopleOutline />
                     <p>Publisher: {publisher}</p>
@@ -41,9 +41,9 @@ const SingleBook = ({book}) => {
                 </div>
             </div>
             <div className="divider"></div>
-            <div className="flex gap-4 font-semibold text-lg">
+            <div className="flex gap-4 flex-wrap font-semibold text-base">
                 <div className="p-2 rounded-3xl bg-[#328EFF26] text-[#328EFF]">Category: {category}</div>
-                <div className="p-2 rounded-3xl text-[#FFAC33] bg-[#FFAC3326]">Rating: {rating}</div>
+                <div className="p-2 rounded-3xl text-[#e6a647] bg-[#FFAC3326]">Rating: {rating}</div>
                 <Link to={`/book/${id}`}><div className="p-2 btn rounded-3xl text-white bg-[#23BE0A]">View Details</div></Link>
             </div>
             
