f849edd78aad3a5802eae2c4fec40a034c1aa74c
NaimoonJannat <naimoonjannat@gmail.com>
2024-03-27 09:53:25 +0600

    Navlink active class fixed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,31 +8,45 @@ const Header = () => {
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
-      <ul tabIndex={0} className="text-lg menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/listedBooks">Listed Books</NavLink></li>
-        <li><NavLink to="pagesToRead">Pages to Read</NavLink></li>
-        <li><NavLink to="/reviews">Reviews</NavLink></li>
-        <li><NavLink to="/contact">Contact</NavLink></li>
+      <ul tabIndex={0} className="text-base menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li><NavLink to="/" className={({ isActive }) =>
+                      isActive ? 'text-[#23BE0A] bg-white border-2 border-[#23BE0A]' : 'font-bold'
+                               }>Home</NavLink></li>
+        <li><NavLink to="/listedBooks" className={({ isActive }) =>
+                      isActive ? 'text-[#23BE0A] bg-white border-2 border-[#23BE0A]' : 'font-bold'
+                               }>Listed Books</NavLink></li>
+        <li><NavLink to="pagesToRead" className={({ isActive }) =>
+                      isActive ? 'text-[#23BE0A] bg-white border-2 border-[#23BE0A]' : 'font-bold'
+                               }>Pages to Read</NavLink></li>
+        <li><NavLink to="/reviews" className={({ isActive }) =>
+                      isActive ? 'text-[#23BE0A] bg-white border-2 border-[#23BE0A]' : 'font-bold'
+                               }>Reviews</NavLink></li>
+        <li><NavLink to="/contact" className={({ isActive }) =>
+                      isActive ? 'text-[#23BE0A] bg-white border-2 border-[#23BE0A]' : 'font-bold'
+                               }>Contact</NavLink></li>
         
       </ul>
     </div>
     <a className="btn btn-ghost text-3xl font-semibold">Book Vibe</a>
   </div>
   <div className="navbar-center hidden lg:flex">
-    <ul className="text-lg menu menu-horizontal px-1">
-    <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink className={({ isActive, isPending }) =>
-                      isActive
-                        ? "active"
-                        : isPending
-                        ? "pending"
-                        : ""
-                        
-                    } to="/listedBooks">Listed Books</NavLink></li>
-        <li><NavLink to="pagesToRead">Pages to Read</NavLink></li>
-        <li><NavLink to="/reviews">Reviews</NavLink></li>
-        <li><NavLink to="/contact">Contact</NavLink></li>
+    <ul className="text-base menu menu-horizontal px-1">
+    <li><NavLink to="/" className={({ isActive }) =>
+                      isActive ? 'text-[#23BE0A] bg-white border-2 border-[#23BE0A]' : 'font-bold'
+                               }>Home</NavLink></li>
+
+        <li><NavLink to="/listedBooks" className={({ isActive }) =>
+                      isActive ? 'text-[#23BE0A] bg-white border-2 border-[#23BE0A]' : 'font-bold'
+                               }>Listed Books</NavLink></li>
+        <li><NavLink to="pagesToRead" className={({ isActive }) =>
+                      isActive ? 'text-[#23BE0A] bg-white border-2 border-[#23BE0A]' : 'font-bold'
+                               }>Pages to Read</NavLink></li>
+        <li><NavLink to="/reviews" className={({ isActive }) =>
+                      isActive ? 'text-[#23BE0A] bg-white border-2 border-[#23BE0A]' : 'font-bold'
+                               }>Reviews</NavLink></li>
+        <li><NavLink to="/contact"  className={({ isActive }) =>
+                      isActive ? 'text-[#23BE0A] bg-white border-2 border-[#23BE0A]' : 'font-bold'
+                               }>Contact</NavLink></li>
       
     </ul>
   </div>
--- src/Components/ListedBooks/ListedBook.jsx
+++ src/Components/ListedBooks/ListedBook.jsx
@@ -3,6 +3,11 @@ const ListedBook = () => {
     return (
         <div>
             <h1>Listed Books</h1>
+            <div role="tablist" className="tabs tabs-lifted">
+  <a role="tab" className="tab tab-active">Read</a>
+  <a role="tab" className="tab ">Wishlist</a>
+  
+</div>
         </div>
     );
 };
